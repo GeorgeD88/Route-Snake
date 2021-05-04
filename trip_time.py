f6a5b667f2817f768def5154faf7485efc920725
George Doujaiji <GeorgeDouj88@gmail.com>
2021-05-04 07:44:15 -0400

    Add new file trip_time.py to get net trip time
    This script is a fraction of the heart of the program, which is to be
    able to return "arrive by" dat for routes with more than 2 destinations.

---
+++ trip_time.py
@@ -0,0 +1,26 @@
+# * On Google maps, you can set to arrive by a certain time and see how long the trip will take
+# * or what time you should leave to get there on time. The problem is once you add more destinations
+# * to your trip, suddenly that feature disappears. This program aims to target that specific feature
+# * and fill the gap using a combination of the Distance Matrix API and the Directions API.
+from credentials import *
+import googlemaps  # try this later
+import requests
+import json
+
+
+# Distance Matrix API
+dismatrix_endpoint = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+dismatrix_parameters = {}
+
+# Directions API
+directions_endpoint = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+directions_parameters = {}
+
+
+# returns a series of coordinates as a single string after lossy compression
+def polyline():
+    """
+        Returns a series of coordinates as a single string after lossy compression...
+
+    """
+    pass
