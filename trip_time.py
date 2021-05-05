e346ddb32d45a2cb76fb916b0ce4b5010edb192b
George Doujaiji <GeorgeDouj88@gmail.com>
2021-05-04 20:13:49 -0400

    Create find_plus_code() to easily get plus codes
    This function basically just wraps the search_places() command
    (from find_location.py) with code to automatically search for the place
    and return the plus code extracted from the return data. It expects the
    same parameters as the original search function and forces the
    'plus_code' field into the fields parameter by appending the 'plus_code'
    field to the 'field' parameter or creating the 'field' key with the
    'plus_code' if it doesn't already exist (essentially forcing it).
    Finally the function is called within a return statement, followed by a
    long trail of subscriptables on the end of it so that the plus code is
    instantly and effortlessly extracted then returned all in the same line.

--- trip_time.py
+++ trip_time.py
@@ -2,6 +2,7 @@
 from credentials import *
 from plus_codes import *
 # ========================
+from find_location import *
 import googlemaps  # try this later
 import requests
 import json
@@ -20,3 +21,22 @@ dismatrix_parameters = {}
 # Directions API
 directions_endpoint = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 directions_parameters = {}
+
+
+def find_plus_code(place_name: str, raw_parameters: list = {"fields": ['name', 'plus_code'],  # TODO: review the parameters argument and determine the best default parameters needed for the task at hand
+                 "locationbias": 'circle:%u@%f,%f' % (2000, UCF_COORDS[0], UCF_COORDS[1])}) -> str:
+    """
+    Extracts the plus code of a location from requested data from the Google Place Search API
+        from a search based on the text input and parameters provided.
+
+    # TODO: CONTINUE FILLING OUT THE DOCSTRING LATER
+    Args:
+        place_name (str): [description]
+
+    Returns:
+        str: [description]
+
+    """
+    if 'plus_code' not in raw_parameters['fields']:
+        raw_parameters['fields'].append('plus_code')
+    return search_places(place_name, raw_parameters)['candidates'][0]['plus_code']['global_code']
